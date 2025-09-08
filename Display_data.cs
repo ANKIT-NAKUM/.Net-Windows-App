0c8dfc986da7aa9f84537a744f010828aa2dc69e
Ankit Nakum <ankit.nakum2006@gmail.com>
2025-09-08 15:26:40 +0530

    View Data From Database

---
+++ Display_data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Project
+{
+    public partial class Display_data : Form
+    {
+        string connection = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\DotNet\\Windows_Project\\Registration.mdf;Integrated Security=True";
+        public Display_data()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(connection);
+            con.Open();
+            SqlDataAdapter da= new SqlDataAdapter("select * from Register", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+            con.Close();
+           
+        }
+    }
+}
